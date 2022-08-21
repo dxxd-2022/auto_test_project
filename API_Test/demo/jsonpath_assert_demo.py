329e187f9e2947ea902e7432ec8bf874abb99ffe
dxxd-2022 <103001053+dxxd-2022@users.noreply.github.com>
2022-08-21 21:59:39 +0800

    Delete API_Test/demo directory

--- API_Test/demo/jsonpath_assert_demo.py
+++
@@ -1,7 +0,0 @@
-import requests
-from jsonpath import jsonpath
-url="https://home.testing-studio.com/categories.json"
-def test_jsonpath():
-    r=requests.get(url=url)
-    print(r.json(),'$..name')
-    assert jsonpath(r.json(),'$..name')[0]=='提问区'
--- API_Test/demo/up_file.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-files={"file":open('file','rb')}
-url="上传文件接口"
-def test_up_file():
-    re=requests.post(url=url,files=files)
